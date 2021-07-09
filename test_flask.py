455297765883fb3669a5130f0d322dec03338b94
nano <i.salah-eddine@adtp.com>
2021-07-09 13:52:54 +0200

    modification test_falsk

--- test_flask.py
+++ test_flask.py
@@ -1,7 +1,7 @@
 import requests
 
 resp = requests.post("http://localhost:5000/predict",
-                     files={"file": open('../70.jpeg', 'rb'),
+                     files={"file": open('../101.jpeg', 'rb'),
                             },
                      data={'detector': 'detector_1'}
                      )
